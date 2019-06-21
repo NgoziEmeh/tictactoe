627d7882af333b4237d14c0bb68daf942ed63dde
Ngozi Emeh <zingo1ng@yahoo.com>
2019-06-21 12:49:11 +0100

    first pull

---
+++ game.py
@@ -0,0 +1,13 @@
+# Make a play board
+inner = ['', '', '', '', '', '', '', '', '', ]
+
+
+def play_board():
+    print(inner[0] + ' | ' + inner[1] + ' | ' + inner[2])
+    print('------')
+    print(inner[3] + ' | ' + inner[4] + ' | ' + inner[5])
+    print('------')
+    print(inner[6] + ' | ' + inner[7] + ' | ' + inner[8])
+
+
+play_board()
